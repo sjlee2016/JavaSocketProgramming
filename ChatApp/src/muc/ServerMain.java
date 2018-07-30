22d8e4501576de224c6c097d85bd355dae27dd60
sjlee2016 <sjlee2016@sogang.ac.kr>
2018-07-30 12:10:58 +0900

    revised the class to OOP style

--- ChatApp/src/muc/ServerMain.java
+++ ChatApp/src/muc/ServerMain.java
@@ -19,37 +19,20 @@ public class ServerMain {
 			{
 				System.out.println("About to accept client connection..");
 				Socket clientSocket = serverSocket.accept();
+				// accept incoming connection
 				System.out.println("Accepted connection from" + clientSocket);
-				Thread t = new Thread() { // create a new thread for each new connection
-					public void run(){
-						try {
-							handleClientSocket(clientSocket);
-							
-						}catch(IOException e)
-						{
-							e.printStackTrace();
-						}
-						catch(InterruptedException e)
-						{
-							e.printStackTrace();
-						}
-					}
-				};
-				}
+				ServerWorker worker = new ServerWorker(clientSocket);
+				// creates serverWorker which is a thread that communicates with clients
+				
+				worker.start();
+			}
+			
 		}catch(IOException e)
 		{
 			e.printStackTrace();
 		}
 	}
 
-	private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
-		OutputStream outputStream = clientSocket.getOutputStream();
-		for(int i = 0 ; i<10; i++)
-		{
-			outputStream.write(("Time now is " + new Date() + "\n").getBytes());
-			Thread.sleep(1000);
-		}
-		clientSocket.close();
-	}
+	
 
 }
--- ChatApp/src/muc/ServerWorker.java
+++ ChatApp/src/muc/ServerWorker.java
@@ -1,10 +1,36 @@
 package muc;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Date;
 
-public class ServerWorker {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class ServerWorker extends Thread {
 
+	private final Socket clientSocket;
+	
+	public ServerWorker(Socket clientSocket)
+	{
+		this.clientSocket = clientSocket;
+		
+	}
+	public void run()
+	{
+		try {
+			handleClientSocket();
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private void handleClientSocket() throws IOException, InterruptedException {
+		OutputStream outputStream = clientSocket.getOutputStream();
+		for(int i = 0 ; i<10; i++)
+		{
+			outputStream.write(("Time now is " + new Date() + "\n").getBytes());
+			Thread.sleep(1000);
+		}
+		clientSocket.close();
 	}
 
 }
