98256add209f5f966000846dd3b39b8587c437c4
sjlee2016 <sjlee2016@sogang.ac.kr>
2018-07-30 12:20:00 +0900

    read input from client and echo back until received 'quit'

--- ChatApp/src/muc/ServerWorker.java
+++ ChatApp/src/muc/ServerWorker.java
@@ -1,6 +1,5 @@
 package muc;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.util.Date;
 
@@ -25,10 +24,20 @@ public class ServerWorker extends Thread {
 	}
 	private void handleClientSocket() throws IOException, InterruptedException {
 		OutputStream outputStream = clientSocket.getOutputStream();
-		for(int i = 0 ; i<10; i++)
+		InputStream inputStream = clientSocket.getInputStream();
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		String line;
+		while((line=reader.readLine())!=null)
 		{
-			outputStream.write(("Time now is " + new Date() + "\n").getBytes());
-			Thread.sleep(1000);
+			if("quit".equalsIgnoreCase(line))  // stop connection when received "quit"
+			{
+				break;
+			}
+			// else echo back to the client
+			
+			String msg = "You typed : " + line + "\n";
+			outputStream.write(msg.getBytes());
 		}
 		clientSocket.close();
 	}
