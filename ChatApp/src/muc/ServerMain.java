e170dff92425293daac3cd5d675431fd02c3a9e3
sjlee2016 <sjlee2016@sogang.ac.kr>
2018-07-30 11:56:26 +0900

    multithread to allow multiple client connection

--- ChatApp/src/muc/ServerMain.java
+++ ChatApp/src/muc/ServerMain.java
@@ -5,12 +5,14 @@ import java.net.ServerSocket;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Date;
 
 public class ServerMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int port = 8820;
+		int port = 8820
+				;
 		try {
 			ServerSocket serverSocket = new ServerSocket(port);
 			while(true)
@@ -18,15 +20,36 @@ public class ServerMain {
 				System.out.println("About to accept client connection..");
 				Socket clientSocket = serverSocket.accept();
 				System.out.println("Accepted connection from" + clientSocket);
-				OutputStream outputStream = clientSocket.getOutputStream();
-				outputStream.write("Hello World\n".getBytes());
-				clientSocket.close(); 
-			}
-			
-		} catch (IOException e)
+				Thread t = new Thread() { // create a new thread for each new connection
+					public void run(){
+						try {
+							handleClientSocket(clientSocket);
+							
+						}catch(IOException e)
+						{
+							e.printStackTrace();
+						}
+						catch(InterruptedException e)
+						{
+							e.printStackTrace();
+						}
+					}
+				};
+				}
+		}catch(IOException e)
 		{
 			e.printStackTrace();
 		}
+	}
+
+	private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+		OutputStream outputStream = clientSocket.getOutputStream();
+		for(int i = 0 ; i<10; i++)
+		{
+			outputStream.write(("Time now is " + new Date() + "\n").getBytes());
+			Thread.sleep(1000);
+		}
+		clientSocket.close();
 	}
 
 }
---
+++ ChatApp/src/muc/ServerWorker.java
@@ -0,0 +1,10 @@
+package muc;
+
+public class ServerWorker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
